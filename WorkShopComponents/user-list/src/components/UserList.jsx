5245765ad76e3555404601080716dad75a98680f
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-11-07 20:01:00 +0000

    Update01

--- WorkShopComponents/user-list/src/components/UserList.jsx
+++ WorkShopComponents/user-list/src/components/UserList.jsx
@@ -1,8 +1,13 @@
+import Search from './Search'
+import UserListTable from './UserListTable';
+
 const UserList = () => {
     return(
       
         <section className="card users-container">
             <Search />
+
+            <UserListTable />
         </section>
 )
 };
