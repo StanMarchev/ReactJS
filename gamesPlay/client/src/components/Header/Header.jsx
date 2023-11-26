65fc61d778bc3907760dd6eb9b96b07f96a557a9
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-11-26 14:56:27 +0000

    get all games service method

--- gamesPlay/client/src/components/Header/Header.jsx
+++ gamesPlay/client/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
 
-            <h1><Link class="home" to="/">GamesPlay</Link></h1>
+            <h1><Link className="home" to="/">GamesPlay</Link></h1>
             <nav>
                 <Link to="/games">All games</Link>
 
--- gamesPlay/client/src/components/game-create/gameCreate.jsx
+++ gamesPlay/client/src/components/game-create/gameCreate.jsx
@@ -1,15 +1,29 @@
-export default function GameCreate () {
-    const createGameSumbitHandler = (e) => {
+import { useNavigate } from 'react-router-dom';
+
+import * as gameService from '../../services/gameService';
+
+export default function GameCreate() {
+    const navigate = useNavigate();
+    
+    const createGameSubmitHandler = async (e) => {
         e.preventDefault();
 
         const gameData = Object.fromEntries(new FormData(e.currentTarget));
+
+        try {
+            await gameService.create(gameData);
+
+            navigate('/games');
+        } catch (err) {
+            // Error notification
+            console.log(err);
+        }
     }
-    
+
     return (
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" onSubmit={createGameSubmitHandler}>
                 <div className="container">
-
                     <h1>Create Game</h1>
                     <label htmlFor="leg-title">Legendary title:</label>
                     <input type="text" id="title" name="title" placeholder="Enter game title..." />
