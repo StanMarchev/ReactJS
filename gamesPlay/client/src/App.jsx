9cbf91ed4854583d8f0bf48077e778fa8d2429ec
Stanimir Marchev <114108086+StanMarchev@users.noreply.github.com>
2023-11-26 12:42:13 +0000

    add Home

--- gamesPlay/client/src/App.jsx
+++ gamesPlay/client/src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header"
+import Home from "./components/Home/Home"
 
 function App() {
 
   return (
     <div id="box">
       <Header />
+      
+      <Home />
    </div>
   )
 }
